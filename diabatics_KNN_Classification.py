903e00e98f27d4b531655c2f9cafae9f34f25842
PremKumar Kora <premkumar.kora@gmail.com>
2023-07-13 14:02:23 +0530

    comments Added

--- diabatics_KNN_Classification.py
+++ diabatics_KNN_Classification.py
@@ -22,4 +22,4 @@ print("Testing accuracy: ",accuracy_score(y_test,y_test_pred))
 print("Training Accuracy: ",accuracy_score(y_train,y_train_pred))
 print(classification_report(y_test,y_test_pred))
 
-# This is the KNN classification File
+# This is the KNN classification File This is chech the GIT checkout to branch
